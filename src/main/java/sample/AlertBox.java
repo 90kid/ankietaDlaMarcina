d09bb4dcc8e1985523b28d8bcc13391eddd78751
Dawid Koziej <januszakahipis@wp.pl>
2019-09-11 14:31:49 +0200

    add AlertBox

---
+++ src/main/java/sample/AlertBox.java
@@ -0,0 +1,35 @@
+package sample;
+
+import javafx.animation.PauseTransition;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import javafx.util.Duration;
+
+import java.io.IOException;
+
+class AlertBox {
+    static void display(){
+
+        try {
+            PauseTransition delay = new PauseTransition(Duration.seconds(1));
+            FXMLLoader fxmlLoader = new FXMLLoader(AlertBox.class.getResource("/AlertBox.fxml"));
+            Parent root1 = fxmlLoader.load();
+            Stage stage = new Stage();
+            delay.setOnFinished( event -> stage.close() );
+            delay.play();
+            stage.setScene(new Scene(root1));
+            stage.setMaximized(true);
+            stage.setResizable(false);
+            stage.initStyle(StageStyle.UTILITY);
+            stage.initModality(Modality.APPLICATION_MODAL);
+            stage.showAndWait();
+
+        } catch (IOException e) {
+            System.err.println("Nie mozna otworzyc okna");
+        }
+    }
+}
---
+++ src/main/java/sample/AlertBoxController.java
@@ -0,0 +1,14 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class AlertBoxController {
+    @FXML
+    private Label text;
+
+    @FXML
+    void initialize(){
+        text.setText("Dziękujemy");
+    }
+}
